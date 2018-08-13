1bfe297c413e684b3bf7510fe3a30f255f404cde
Nelson Costa <nelsonscosta@yahoo.com.br>
2018-08-13 12:37:00 -0300

    Primeiro commit

---
+++ src/br/com/conexao/ConectaMySQL.java
@@ -0,0 +1,24 @@
+package br.com.conexao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConectaMySQL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Connection conexao = null;
+		try{
+			String url = "jdbc:mysql://localhost/agenda?useTimezone=true&serverTimezone=UTC";
+			String usuario = "root";
+			String senha = "Mtsa972106";
+			conexao  = DriverManager.getConnection(url, usuario, senha);
+			System.out.println("Conectou");
+			conexao.close();
+		}catch(SQLException e){
+			System.out.println("Ocorreu um erro ao criar a conexão. Erro: "+ e.getMessage());
+		}
+		
+	}
+
+}
